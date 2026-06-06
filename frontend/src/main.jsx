41104d33ba87c519655286c799635030c3cac5ec
FilipeCampos25 <lipecduarte255@gmail.com>
2026-06-06 11:29:48 -0300

    style: create responsive layout styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import './styles/navbar.css';
 import './styles/home.css';
 import './styles/form.css';
 import './styles/list.css';
+import './styles/responsive.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
